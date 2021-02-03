5868f62c87b15c1480aa1251a59855bc2bb3a6eb
jrandiny <jrandiny@gmail.com>
2021-02-03 09:20:14 +0700

    Add slingshot script

---
+++ minggu2/Assets/Scripts/SlingShooter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class SlingShooter : MonoBehaviour
+{
+    public CircleCollider2D circleCollider2D;
+    private Vector2 _startPosition;
+
+    [SerializeField] private float _pullRadius = 0.75f;
+    [SerializeField] private float _throwSpeed = 30f;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        circleCollider2D.enabled = false;
+        var currentPosition = (Vector2) transform.position;
+        var velocity = _startPosition - currentPosition;
+        var distance = Vector2.Distance(_startPosition, currentPosition);
+
+        transform.position = _startPosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        var mousePosition = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        var diff = Vector2.ClampMagnitude(mousePosition - _startPosition, _pullRadius);
+
+        transform.position = _startPosition + diff;
+    }
+}
