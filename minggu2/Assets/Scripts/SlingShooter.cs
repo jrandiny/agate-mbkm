792007da965f9c921f9ca71c271a5787c93e696b
jrandiny <jrandiny@gmail.com>
2021-02-03 21:03:09 +0700

    Add rope

--- minggu2/Assets/Scripts/SlingShooter.cs
+++ minggu2/Assets/Scripts/SlingShooter.cs
@@ -8,8 +8,14 @@ public class SlingShooter : MonoBehaviour
 {
     public CircleCollider2D circleCollider2D;
     public LineRenderer trajectory;
+    public LineRenderer backRope;
+    public LineRenderer frontRope;
+
+    [SerializeField] private float birdRadius;
     private Vector2 _startPosition;
     private BirdController _bird;
+    private Vector3 _backRopeInitialPosition;
+    private Vector3 _frontRopeInitialPosition;
 
     [SerializeField] private float pullRadius = 0.75f;
     [SerializeField] private float throwSpeed = 30f;
@@ -17,6 +23,8 @@ public class SlingShooter : MonoBehaviour
     void Start()
     {
         _startPosition = transform.position;
+        _backRopeInitialPosition = backRope.GetPosition(1);
+        _frontRopeInitialPosition = frontRope.GetPosition(1);
     }
 
     private void OnMouseUp()
@@ -31,6 +39,9 @@ public class SlingShooter : MonoBehaviour
         transform.position = _startPosition;
 
         trajectory.enabled = false;
+
+        backRope.SetPosition(1, _backRopeInitialPosition);
+        frontRope.SetPosition(1, _frontRopeInitialPosition);
     }
 
     private void OnMouseDrag()
@@ -38,19 +49,25 @@ public class SlingShooter : MonoBehaviour
         var mousePosition = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         var diff = Vector2.ClampMagnitude(mousePosition - _startPosition, pullRadius);
-
-        transform.position = _startPosition + diff;
+        var positionTarget = (_startPosition + diff);
+        transform.position = positionTarget;
 
         if (!trajectory.enabled)
         {
             trajectory.enabled = true;
         }
-        float distance = Vector2.Distance(_startPosition, transform.position);
+
         DisplayTrajectory(diff);
+
+        var ropePositionTarget = _startPosition + (diff * ((diff.magnitude + birdRadius) / diff.magnitude));
+
+        frontRope.SetPosition(1, ropePositionTarget);
+        backRope.SetPosition(1, ropePositionTarget);
     }
+
     void DisplayTrajectory(Vector2 diff)
     {
-        if(_bird == null)
+        if (_bird == null)
         {
             return;
         }
@@ -66,7 +83,8 @@ public class SlingShooter : MonoBehaviour
         for (var i = 1; i < segmentCount; i++)
         {
             var elapsedTime = i * Time.fixedDeltaTime * 50 / shootVelocity.magnitude;
-            segments[i] = segments[0] + shootVelocity * elapsedTime + 0.5f * Physics2D.gravity * Mathf.Pow(elapsedTime, 2);
+            segments[i] = segments[0] + shootVelocity * elapsedTime +
+                          0.5f * Physics2D.gravity * Mathf.Pow(elapsedTime, 2);
         }
 
         trajectory.positionCount = segmentCount;
